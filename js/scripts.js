dadaea5176f03553069e67a9e5cb172c81e25550
Student <example@example.com>
2019-12-11 19:38:46 -0800

    add more branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,53 @@
-var food = ['item1', 'item2', 'item3', 'item4'];
+$(document).ready(function() {
+  var food = ['item1', 'item2', 'item3', 'item4'];
+
+});
+
+var list = ['item1', 'item2', 'item3', 'item4'];
+
+sort(food)
+
+
+function check () {
+
+}
+
+document.getElementById("message").innerHTML = list;
+
+
+
+$('#food > li').
+sort(
+  funtion() {
+
+    return food[$('input', item1).val() ] - ordering['input', iteam2).val()];
+  }
+)
+each(
+  funtion() {
+    $('#food').append(this);
+  }
+);
+
+
+
+
+$(document).ready(function() {
+  $("#food").submit(function(event) {
+    var item1 = $("input#item1").val();
+    var item2 = $("input#item2").val();
+    var item3= $("input#item3").val();
+    var item4 = $("input#item4").val();
+
+
+    $(".item1").text(item1Input);
+    $(".item2").text(item2Input);
+    $(".item3").text(item3Input);
+    $(".item4").text(item4Input);
+
+
+    $("#list").show();
+
+    event.preventDefault();
+  });
+});
